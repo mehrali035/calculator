c6017cae307c88ff0473f308be8f3c070fe4dea7
Mehr ali <mehrali035@gmail.com>
2022-11-01 21:40:12 +0100

    calculatoR

---
+++ calcu.js
@@ -0,0 +1,20 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).map((button) => {
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == 'AC'){
+                string = ""
+                document.querySelector('input').value = string;
+              }
+             else{
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+            
+        }
+    })
+});
